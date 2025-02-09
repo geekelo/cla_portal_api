aaf8c671e4bb8b6a5a0e17fa8d9219015091c1f8
geekelo <efueliht12@gmail.com>
2025-02-10 00:59:01 +0100

    Fix bugs

--- app/models/cla_course.rb
+++ app/models/cla_course.rb
@@ -4,7 +4,7 @@ class ClaCourse < ApplicationRecord
   has_many :topics, class_name: 'ClaTopic', foreign_key: 'cla_course_id'
   has_many :assignments, class_name: 'ClaAssignment', foreign_key: 'cla_course_id'
   has_many :live_classes, class_name: 'ClaLiveClass', foreign_key: 'cla_course_id'
-  belongs_to :facilitators, -> {
+  belongs_to :facilitator, -> {
                               joins(:cla_role).where(cla_roles: { name: 'facilitator' })
                             }, class_name: 'ClaUser', foreign_key: 'cla_user_id'
   has_many :cohorts, class_name: 'ClaCohort', foreign_key: 'cla_cohort_id'
