ce4c8413545866e6e5e568ce2ff4e501e7a0b3b5
geekelo <efueliht12@gmail.com>
2025-02-11 03:55:27 +0100

    Fix bugs

--- app/models/cla_submission.rb
+++ app/models/cla_submission.rb
@@ -1,4 +1,3 @@
 class ClaSubmission < ApplicationRecord
-  belongs_to :cla_assignment, class_name: 'ClaAssignment', foreign_key: 'cla_assignment_id'
-  belongs_to :cla_user, class_name: 'ClaUser', foreign_key: 'cla_user_id'
+
 end
