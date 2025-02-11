1e73dcb6a3968d7f0978f6afe649f6d84ca6adb1
geekelo <efueliht12@gmail.com>
2025-02-11 03:05:45 +0100

    Fix bugs

--- app/serializers/cla_submission_serializer.rb
+++ app/serializers/cla_submission_serializer.rb
@@ -1,3 +1,3 @@
 class ClaSubmissionSerializer < ActiveModel::Serializer
-  attributes :id, :download_link, :cla_assignment_id, :cla_user_id, :score, :status, :created_at
+  attributes :id, :download_link, :cla_assignment_id, :cla_student_id, :cla_facilitator_id, :score, :status, :created_at
 end
