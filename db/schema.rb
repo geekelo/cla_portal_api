9c3ba428d4a8c12e428662a9b6e1111e87aa2451
geekelo <efueliht12@gmail.com>
2025-02-11 03:35:41 +0100

    RemoveDateSubmittedAndChangeStatusNullInClaSubmissions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_11_022632) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_11_023412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -83,8 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_11_022632) do
 
   create_table "cla_submissions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "download_link", null: false
-    t.datetime "date_submitted", null: false
-    t.string "status", default: "unmarked", null: false
+    t.string "status", default: "unmarked"
     t.integer "score"
     t.string "cla_assignment_id"
     t.string "cla_student_id"
