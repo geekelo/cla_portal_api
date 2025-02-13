310d21e5e50ea36c6e9d9ada0da653d9c6a36c1f
geekelo <efueliht12@gmail.com>
2025-02-13 22:38:40 +0100

    Fix bugs

--- app/serializers/cla_course_serializer.rb
+++ app/serializers/cla_course_serializer.rb
@@ -1,7 +1,3 @@
 class ClaCourseSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :start_date, :end_date, :created_at, :updated_at
-
-  has_many :topics
-  has_many :assignments
-  has_many :live_classes
 end
