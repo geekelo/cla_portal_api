61e3743cb98103596968c957b43fb31102bf4ac2
geekelo <efueliht12@gmail.com>
2025-02-15 02:24:28 +0100

    Fix bugs

--- app/models/cla_assignment.rb
+++ app/models/cla_assignment.rb
@@ -1,4 +1,4 @@
 class ClaAssignment < ApplicationRecord
   belongs_to :cla_course, class_name: 'ClaCourse', foreign_key: 'cla_course_id'
-  has_many :cla_submissions,  optional: true
+  has_many :submissions, class_name: 'ClaSubmission', foreign_key: 'cla_assignment_id'
 end
