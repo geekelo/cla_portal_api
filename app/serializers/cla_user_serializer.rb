87ae9eaf3a69c7dbcce2f4c98d9d93b34909b0cd
geekelo <efueliht12@gmail.com>
2025-02-15 15:18:38 +0100

    FIx bugs

--- app/serializers/cla_user_serializer.rb
+++ app/serializers/cla_user_serializer.rb
@@ -1,12 +1,20 @@
 class ClaUserSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :email, :cla_cohort_id, :cla_role_id, :name, :phone_number, :birthday,
+  attributes :id, :default_id, :email, :cla_cohort_id, :cla_role_id, :name, :phone_number, :birthday,
              :cla_cohort_name, :cla_role_name
 
+  def id
+    object&.user_id
+  end    
+  
+  def default_id
+    object&.id
+  end    
+
   def cla_cohort_name
-    object.cla_cohort&.name
+    object&.cla_cohort&.name
   end
 
   def cla_role_name
-    object.cla_role&.name
+    object&.cla_role&.name
   end
 end
