3ac5d35810171a6176887056a0885281d74faa20
geekelo <efueliht12@gmail.com>
2025-02-17 00:46:59 +0100

    Fix lint

--- app/helpers/cla_dashboard_student_list_helper.rb
+++ app/helpers/cla_dashboard_student_list_helper.rb
@@ -1,18 +1,19 @@
 module ClaDashboardStudentListHelper
   def self.student_list(cohort_id)
-    cohort_id = cohort_id.to_s # Ensure cohort_id is treated as a string
+    cohort_id = cohort_id.to_s  # Ensure cohort_id is treated as a string
 
     # Get all students in the cohort
-    ClaUser.where('cla_cohort_id::text = ?', cohort_id)
-      .where(role: 'student')
-      .order(:first_name, :last_name)
-      .map do |student|
-      {
-        user_id: student.id,
-        first_name: student.first_name,
-        last_name: student.last_name,
-        email: student.email
-      }
-    end
+    students = ClaUser.where("cla_cohort_id::text = ?", cohort_id)
+                      .where(cla_role: { name: 'student' })
+                      .order(:name)
+                      .map do |student|
+                        {
+                          user_id: student.user_id,
+                          name: student.name,
+                          email: student.email
+                        }
+                      end
+
+    students
   end
 end
