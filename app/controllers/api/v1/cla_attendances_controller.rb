793c869a645a94669cbe9e000d82aacf58a40546
geekelo <efueliht12@gmail.com>
2025-02-16 19:01:08 +0100

    Fix bugs

--- app/controllers/api/v1/cla_attendances_controller.rb
+++ app/controllers/api/v1/cla_attendances_controller.rb
@@ -19,15 +19,19 @@ module Api
       
         # Get all users in the cohort
         cohort_users = ClaUser.where(cla_cohort_id: cla_cohort_id)
+        Rails.logger.info "Cohort Users: #{cohort_users.pluck(:id, :cla_cohort_id)}"
       
         # Get user IDs already marked in attendance for this live class
         recorded_user_ids = ClaAttendance.where(cla_live_class_id: cla_live_class_id).pluck(:cla_user_id).map(&:to_s)
+        Rails.logger.info "Recorded User IDs: #{recorded_user_ids}"
       
-        # Get users who do NOT exist in the attendance table for the given live class
+        # Get users who have NOT been marked in attendance
         missing_users = cohort_users.where.not(id: recorded_user_ids.map(&:to_s))
+        Rails.logger.info "Missing Users: #{missing_users.pluck(:id)}"
       
         render json: { missing_users: missing_users }, status: :ok
       end
+      
       
   
 
