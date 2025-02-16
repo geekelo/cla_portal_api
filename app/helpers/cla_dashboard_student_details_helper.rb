d7529cbd37b2d193b407e9eb7146bb21c1baf5df
geekelo <efueliht12@gmail.com>
2025-02-17 00:30:41 +0100

    Fix lint

--- app/helpers/cla_dashboard_student_details_helper.rb
+++ app/helpers/cla_dashboard_student_details_helper.rb
@@ -3,19 +3,20 @@ module ClaDashboardStudentDetailsHelper
     user_id = user_id.to_s  # Ensure user_id is treated as a string
 
     # Get student details
-    student = ClaUser.where("user_id::text = ?", user_id)
-                     .where(role: 'student')
-                     .map do |student|
-                       {
-                         user_id: student.user_id,
-                         name: student.name,
-                         email: student.email,
-                         cohort_id: student.cohort_id&.name,
-                         role: student.cla_role&.name,
-                         phone_number: student.phone_number,
+    students = ClaUser.where("user_id::text = ?", user_id)
+                      .where(role: 'student')
+                      .map do |student|
+                        {
+                          user_id: student.user_id,
+                          name: student.name,
+                          email: student.email,
+                          cohort_id: student.cohort_id&.name,
+                          role: student.cla_role&.name,
+                          phone_number: student.phone_number,
                           birthday: student.birthday
                         }
+                      end
 
-    student
+    students
   end
 end
