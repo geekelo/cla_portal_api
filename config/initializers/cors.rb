71c630b2301f0c3eda8eecc37c722e56bd000583
geekelo <efueliht12@gmail.com>
2025-02-17 21:57:50 +0100

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://localhost:3001'] # Add your frontend origin(s)
+    origins ['https://cla.jjrsf.org'] # Add your frontend origin(s)
 
     resource '/api/*',
       headers: :any,
