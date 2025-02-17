778f1696a0628355a408a143960eccad7d5d82bb
geekelo <efueliht12@gmail.com>
2025-02-17 01:04:18 +0100

    Combine student list and student details

--- app/controllers/api/v1/cla_dashboards_controller.rb
+++ app/controllers/api/v1/cla_dashboards_controller.rb
@@ -42,13 +42,34 @@ module Api
         render json: stats, status: :ok
       end
 
-      def student_list
+      def students_with_details
         cohort_id = params[:cla_cohort_id]
         return render json: { error: 'Cohort ID is required' }, status: :unprocessable_entity unless cohort_id
-
+      
         students = ClaDashboardStudentListHelper.student_list(cohort_id)
-        render json: students, status: :ok
+      
+        students_with_details = students.map do |student|
+          user_id = student[:user_id]
+      
+          {
+            student: student,
+            course_completion_rate: ClaDashboardCourseStatsHelper.course_completion_rate(cohort_id, user_id),
+            user_score_percentage: ClaDashboardScoreStatsHelper.user_score_percentage(user_id),
+            user_submission_percentage: ClaDashboardAssignmentStatsHelper.user_submission_percentage(user_id),
+            user_attendance_percentage: ClaDashboardAttendanceStatsHelper.user_attendance_percentage(user_id)
+          }
+        end
+      
+        render json: students_with_details, status: :ok
       end
+      
+      # def student_list
+      #   cohort_id = params[:cla_cohort_id]
+      #   return render json: { error: 'Cohort ID is required' }, status: :unprocessable_entity unless cohort_id
+
+      #   students = ClaDashboardStudentListHelper.student_list(cohort_id)
+      #   render json: students, status: :ok
+      # end
 
       def student_details
         cohort_id = params[:cla_cohort_id]
