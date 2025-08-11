a8e4b4a58daaa0ede165ef865bf3daa1c8124169
Eloghene <eloghene@bananacrystal.com>
2025-08-11 04:28:23 +0100

    Fix migration

--- db/migrate/20250811000000_fix_cla_contributions_cohort_id_type.rb
+++ db/migrate/20250811000000_fix_cla_contributions_cohort_id_type.rb
@@ -3,8 +3,9 @@ class FixClaContributionsCohortIdType < ActiveRecord::Migration[7.1]
     # Remove the existing index first
     remove_index :cla_contributions, name: 'index_cla_contributions_on_course_user_cohort'
     
-    # Change the column type from UUID to integer
-    change_column :cla_contributions, :cla_cohort_id, :bigint, null: true
+    # Drop and recreate the column with the correct type
+    remove_column :cla_contributions, :cla_cohort_id
+    add_column :cla_contributions, :cla_cohort_id, :integer, null: true
     
     # Recreate the index with the new column type
     add_index :cla_contributions, [:cla_course_id, :cla_user_id, :cla_cohort_id], unique: true, name: 'index_cla_contributions_on_course_user_cohort'
@@ -14,8 +15,9 @@ class FixClaContributionsCohortIdType < ActiveRecord::Migration[7.1]
     # Remove the index
     remove_index :cla_contributions, name: 'index_cla_contributions_on_course_user_cohort'
     
-    # Change the column type back to UUID
-    change_column :cla_contributions, :cla_cohort_id, :uuid, null: true
+    # Drop and recreate the column with UUID type
+    remove_column :cla_contributions, :cla_cohort_id
+    add_column :cla_contributions, :cla_cohort_id, :uuid, null: true
     
     # Recreate the index
     add_index :cla_contributions, [:cla_course_id, :cla_user_id, :cla_cohort_id], unique: true, name: 'index_cla_contributions_on_course_user_cohort'
