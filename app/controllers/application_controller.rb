d743adf18c64648850fc87b9bf07b332e257f8b5
Eloghene <eloghene@bananacrystal.com>
2025-08-11 14:56:55 +0100

    Add authentication to API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
-class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  private
+
+  def authenticate_user!
+    payload = JsonWebToken.decode(auth_token)
+    @current_user = ClaUser.find(payload["sub"])
+  rescue JWT::DecodeError
+    render json: { error: "Invalid auth token" }, status: :unauthorized
+  end
+
+  def auth_token
+    @auth_token ||= request.headers["Authorization"].to_s.split.last
+  end
 end
--- app/serializers/cla_contributions_serializer.rb
+++ app/serializers/cla_contributions_serializer.rb
@@ -2,10 +2,14 @@ class ClaContributionsSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :due_date, :cla_cohort_id, :cla_course_id, :student_score
 
   def student_score
-    if current_user.cla_role.name == 'student'
-      object.cla_contributions_scores.find_by(cla_user_id: current_user.id).score
+    return nil unless current_user
+    
+    if current_user.cla_role&.name == 'student'
+      score_record = object.cla_contributions_scores.find_by(cla_user_id: current_user.id)
+      score_record&.score
     else
-      object.cla_contributions_scores.find_by(cla_user_id: object.cla_user_id).score
+      score_record = object.cla_contributions_scores.find_by(cla_user_id: object.cla_user_id)
+      score_record&.score
     end
   end
 end
