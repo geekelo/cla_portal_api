e1c9c5c7acaaee3367fa7fef4b39b48bb9eefc52
Eloghene <eloghene@bananacrystal.com>
2025-08-11 02:25:21 +0100

    Filter contributions by cohort

--- app/controllers/api/v1/cla_contributions_controller.rb
+++ app/controllers/api/v1/cla_contributions_controller.rb
@@ -2,24 +2,22 @@ class Api::V1::ClaContributionsController < ApplicationController
   def index
     contributions = if params[:cla_course_id].present?
                       ClaContribution.where(cla_course_id: params[:cla_course_id])
-                    elsif params[:cla_user_id].present?
-                      ClaContribution.where(cla_user_id: params[:cla_user_id])
                     else
-                      ClaContribution.all
+                      ClaContribution.where(cla_cohort_id: params[:cla_cohort_id])
                     end.order(created_at: :desc)
 
-    render json: contributions, each_serializer: ClaContributionSerializer, status: :ok
+    render json: contributions, each_serializer: ClaContributionsSerializer, status: :ok
   end
 
   def show
     contribution = ClaContribution.find(params[:id])
-    render json: contribution, each_serializer: ClaContributionSerializer, status: :ok
+    render json: contribution, each_serializer: ClaContributionsSerializer, status: :ok
   end
 
   def create
     contribution = ClaContribution.new(contribution_params)
     if contribution.save
-      render json: contribution, each_serializer: ClaContributionSerializer, status: :created
+      render json: contribution, each_serializer: ClaContributionsSerializer, status: :created
     else
       render json: { errors: contribution.errors.full_messages }, status: :unprocessable_entity
     end
@@ -28,7 +26,7 @@ class Api::V1::ClaContributionsController < ApplicationController
   def update
     contribution = ClaContribution.find(params[:id])
     if contribution.update(contribution_params)
-      render json: contribution, each_serializer: ClaContributionSerializer, status: :ok
+      render json: contribution, each_serializer: ClaContributionsSerializer, status: :ok
     else
       render json: { errors: contribution.errors.full_messages }, status: :unprocessable_entity
     end
