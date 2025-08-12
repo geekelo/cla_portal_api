49f3868df7475b334c174fc7a226ceb36e77df32
Eloghene <eloghene@bananacrystal.com>
2025-08-12 18:42:15 +0100

    Refactor code to avoid errors

--- app/serializers/cla_cbt_serializer.rb
+++ app/serializers/cla_cbt_serializer.rb
@@ -3,11 +3,11 @@ class ClaCbtSerializer < ActiveModel::Serializer
 
   def student_score
     return nil unless current_user
-    
+
     if current_user.cla_role.name == 'student'
-      object.cla_cbts_scores.find_by(cla_user_id: current_user.id).score
+      object&.cla_cbts_scores&.find_by(cla_user_id: current_user.id)&.score
     else
-      object.cla_cbts_scores.find_by(cla_user_id: object.cla_user_id).score
+      nil
     end
   end
 end
