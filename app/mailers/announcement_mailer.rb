9e8d489b251dc522e325cb145a04c42796637021
Eloghene <eloghene@bananacrystal.com>
2025-08-13 20:42:58 +0100

    Add comments

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -1,19 +1,11 @@
 class AnnouncementMailer < ApplicationMailer
-  def send_new_announcement(announcement)
+  def new_announcement(announcement, students)
     @announcement = announcement
+    @students = students
     
     # Send to all students in the cohort
-    if announcement.cla_user_id
-      student = ClaUser.find_by(user_id: announcement.cla_user_id)
+    @students.each do |student|
       mail(
-        to: student.email,
-        subject: "New Announcement: #{@announcement.title}",
-        from: 'jjrsfcla@gmail.com'
-      )
-    else
-      students = ClaUser.where(cla_cohort_id: announcement.cla_cohort_id)
-      students.each do |student|
-        mail(
         to: student.email,
         subject: "New Announcement: #{@announcement.title}",
         from: 'jjrsfcla@gmail.com'
