4fc462fcdeef5447dc4df8885952edf29e7b6849
Eloghene <eloghene@bananacrystal.com>
2025-08-18 15:31:44 +0100

    Make submission unique

--- app/models/cla_submission.rb
+++ app/models/cla_submission.rb
@@ -3,4 +3,7 @@ class ClaSubmission < ApplicationRecord
   belongs_to :cla_cohort, optional: true
   belongs_to :cla_course, optional: true
   belongs_to :cla_student, class_name: 'ClaUser', foreign_key: 'cla_student_id'
+
+  # validate that a student can only submit an assignment once
+  validates :cla_student_id, uniqueness: { scope: :cla_assignment_id, message: 'Student has already been scored for this assignment' }
 end
