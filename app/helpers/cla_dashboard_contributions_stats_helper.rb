7243d99ba256b65d24fb81d1c2bd340e70be40fd
Eloghene <eloghene@bananacrystal.com>
2025-09-03 15:34:53 +0100

    Fix bugs

--- app/helpers/cla_dashboard_contributions_stats_helper.rb
+++ app/helpers/cla_dashboard_contributions_stats_helper.rb
@@ -12,7 +12,9 @@ module ClaDashboardContributionsStatsHelper
     # Get total contributions scores of user
     total_contributions_scores = ClaContributionsScore.where(cla_user_id: user_id, cla_course_id: course_id).sum(:score)
 
-    total_contributions_percentage = total_contributions_scores.zero? ? 0 : (total_contributions_scores.to_f / (total_contributions.to_f * 100) * 100).round(2)
+    # Calculate percentage: (user's total score) / (number of attempted contributions × 100) × 100
+    attempted_contributions = ClaContributionsScore.where(cla_user_id: user_id, cla_course_id: course_id).count
+    total_contributions_percentage = attempted_contributions.zero? ? 0 : (total_contributions_scores.to_f / (attempted_contributions.to_f * 100) * 100).round(2)
 
     # Get contributions points
     contributions_points = ((total_contributions_percentage * 25)/100).round(2)
