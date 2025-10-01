f7db1aa4355e31b2dbb41ee8d29249d7f5966bfa
Eloghene <eloghene@bananacrystal.com>
2025-10-01 10:27:31 +0100

    Update default from: for emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-default from: "jjrsfcla@gmail.com"  # Change this to your preferred sender email
+default from: "JJRSF CLA Team <team@jjrsf.org>"  # Change this to your preferred sender email
   layout "mailer"
 end
