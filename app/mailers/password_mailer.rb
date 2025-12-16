359d97c599ede1e6a90e0f0e8e192792b772ecaf
Eloghene <eloghene@bananacrystal.com>
2025-12-16 21:05:53 +0100

    Update the email

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,6 +1,5 @@
 class PasswordMailer < ApplicationMailer
-    # default from: 'no-reply@cla.jjrsf.org'
-  
+
     def forgot_password
       @user = params[:user]
       @reset_link = "#{Rails.application.credentials.dig(:frontend_url)}/reset-password?token=#{@user.reset_password_token}"
