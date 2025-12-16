f9b5dabe827f18969fb4602c118052a90aa118a1
Eloghene <eloghene@bananacrystal.com>
2025-12-16 20:56:02 +0100

    Update the email

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -4,6 +4,8 @@ class PasswordMailer < ApplicationMailer
     def forgot_password
       @user = params[:user]
       @reset_link = "#{Rails.application.credentials.dig(:frontend_url)}/reset-password?token=#{@user.reset_password_token}"
-      mail(to: @user.email, subject: "Reset Your Password")
+      mail(to: @user.email, 
+      from: "team@jjrsf.org",
+      subject: "Reset Your Password")
     end
   end
